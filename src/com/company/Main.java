568021b75b03102e3b965e1f8cef3be3fd66a578
balabushko <a.balabushko@gillbus.com>
2016-01-25 18:02:03 +0200

    add hw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int array[] = {3,5,7,12,17,7};
+        int array[] = {3,5,7,12,17,7,0};
         int value = array[5];
 
         int sum = Task1.getSumElements(array);
@@ -14,12 +14,14 @@ public class Main {
         int minElemAndIndex = Task3.getMinElement(array);
         double average = Task4.getAverage(array);
         int countValue = Task5.getIndexElementValue(array,value);
+        int countElements = Task6.countElement(array);
 
         System.out.println("Array sum " + sum);
         System.out.println("Max element and index " + maxElemAndIndex);
         System.out.println("Min element and index " + minElemAndIndex);
         System.out.println("Average " + average);
         System.out.println("Preset value " + countValue);
+        System.out.println("count " + countElements);
 
     }
 }
